d2688e61821ebf4b6f5fc24061a0bb08ca99b1d7
Luke Wilson <wilson.palooza@gmail.com>
2016-11-12 12:12:39 -0800

    - Renames server.js to app.js

---
+++ server/app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+
+var routes = require('./routes/routes.js');
+
+var app = express();
+
+//serving static files - will we need this with Angular 2?
+// app.use(express.static(path.join(__dirname, '../app')));
+
+//to parse incoming requests so we can access req.body.whatever
+app.use(bodyParser.json());
+
+//for more verbose server logging
+app.use(logger('dev'));
+
+
+
+//connect to DB
+
+var port = 3002;
+
+app.listen(port, function() {
+  console.log('server is listening on', port);
+})
+
+
+module.exports = app;
