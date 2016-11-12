3a72ca5ddfbf9e94bad9398c04c22b94e6e4693b
Luke Wilson <wilson.palooza@gmail.com>
2016-11-12 14:45:21 -0800

    - renames databaseUrl.js to dbConfig.js - Adds username, password, dbName and url  properties to module.exports

---
+++ server/db/dbConfig.js
@@ -0,0 +1,10 @@
+//this file contains the username and password for our postgreSQL database on elephantsql.com
+// Don't add this to git.
+
+module.exports.username = 'gcgfmakv';
+
+module.exports.password = 'q5sCRhKf8bLDjXTM23DyZ2NB2SdDuejw';
+
+module.exports.dbName = 'gcgfmakv'
+
+module.exports.url = 'postgres://gcgfmakv:q5sCRhKf8bLDjXTM23DyZ2NB2SdDuejw@elmer.db.elephantsql.com:5432/gcgfmakv'
