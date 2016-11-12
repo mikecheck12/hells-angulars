424bbae0165438d6621111919b5c4c09c952c57b
Luke Wilson <wilson.palooza@gmail.com>
2016-11-12 13:56:06 -0800

    - Requires databaseUrl.js (which contains the database credentials)

--- server/db/index.js
+++ server/db/index.js
@@ -1,17 +1,21 @@
 var pg = require('pg'); //node-postgres.  Docs link here: https://www.npmjs.com/package/pg
 var schema = require('./schema');
 
-
+//database URLs. Use localdb if you want to test locally.
+var db_url = require('./databaseUrl.js');
+var localdb = 'postgres://localhost:5432/hells'
 // set database location
-var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/hells';
+var connectionString = process.env.DATABASE_URL || db_url;
+
+
+
 
 //instantiate a new pg client
 var client = new pg.Client(connectionString);
 
 //connect to our database
 client.connect();
+
+//set up the schema
 var query = client.query(schema);
 query.on('end', () => { client.end(); });
-
-
-
