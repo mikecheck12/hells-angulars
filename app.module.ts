c99076c69e2f1e0efc4e8701f116ca7190856cbf
Alex Leo <aleo1337@gmail.com>
2016-11-12 18:35:02 -0500

    Move app module to root folder

---
+++ app.module.ts
@@ -0,0 +1,44 @@
+
+import { NgModule, Component }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { UIRouterModule } from 'ui-router-ng2';
+import { HttpModule } from '@angular/http';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+
+import {App} from "./app/components/app.js";
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/toPromise";
+
+import {Hello} from "./app/components/hello.js";
+import {About} from "./app/components/about.js";
+import {People} from "./app/components/people.js";
+import {Person} from "./app/components/person.js";
+import {PeopleService} from "./app/services/people.js";
+import {MyUIRouterConfig} from "./app/config/router.config.js";
+import {helloState, aboutState, peopleState, personState} from "./states.js";
+
+let INITIAL_STATES =  [ helloState, aboutState, peopleState, personState ];
+let INITIAL_COMPONENTS =  [ App, Hello, About, People, Person ];
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    UIRouterModule.forRoot({
+      states: INITIAL_STATES,
+      useHash: true,
+      configClass: MyUIRouterConfig
+    })
+  ],
+  providers: [ 
+    { provide: PeopleService, useClass: PeopleService },
+  ],
+  declarations: INITIAL_COMPONENTS,
+  bootstrap: [ App ]
+})
+export class AppModule { }
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+    .catch(err => console.error(err));
