400314a61455cba7c919de6d5e4ad25380cb7c8b
Luke Wilson <wilson.palooza@gmail.com>
2016-11-12 13:21:58 -0800

    - Creates db connection (local) and runs schema on connect

--- server/db/index.js
+++ server/db/index.js
@@ -1,1 +1,17 @@
-index.js
+var pg = require('pg'); //node-postgres.  Docs link here: https://www.npmjs.com/package/pg
+var schema = require('./schema');
+
+
+// set database location
+var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/hells';
+
+//instantiate a new pg client
+var client = new pg.Client(connectionString);
+
+//connect to our database
+client.connect();
+var query = client.query(schema);
+query.on('end', () => { client.end(); });
+
+
+
