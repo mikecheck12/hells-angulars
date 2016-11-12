c30d083c9e39275b7a5e99b4799325b1503d6c0c
Luke Wilson <wilson.palooza@gmail.com>
2016-11-12 15:22:25 -0800

    - Adds a couple of test API routes to make sure everything is connected

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,6 +1,24 @@
 var express = require('express');
-var router = express.Router();
-var pg = require('pg');
+
+//require controllers
+var userController = require('../users/userController.js');
+
+
+module.exports = function(app, express) {
+  //User routes
+  app.get('/api/users', userController.getAllUsers);
+  app.post('/api/users', userController.createUser);
+
+  //Product routes
+
+
+  //Review routes
+
+
+  //Transaction routes
+
+
+}
 
 
 
