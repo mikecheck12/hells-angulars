13d48b3ce5f524258b7bc85afeaba82bc89e6001
Luke Wilson <wilson.palooza@gmail.com>
2016-11-12 16:08:42 -0800

    - Adds some database queries to the test methods in userController

--- server/users/userController.js
+++ server/users/userController.js
@@ -1,14 +1,28 @@
+var pool = require('../db/db.js')
+
 module.exports = {
   getAllUsers: function(req, res, next) {
     console.log('getAllUsers fired');
-    res.send('response from getAllUsers');
+    var queryStr = 'SELECT * FROM users';
+    pool.query(queryStr, function(err, result) {
+      if (err) return console.log(err);
+      res.send(result.rows);
+    })
   },
 
   createUser: function(req, res, next) {
-    console.log(req.body);
-    var query = `INSERT INTO users (username, firstname, lastname, email) VALUES ("Lukie", "Luke", "Wilson", "wilson.palooza@gmail.com");`;
+    console.log(req.body); //we need to parse this out and add the info to the queryStr
 
-    res.send("received");
+    // NOTE: you must use single quotes for the values section of the query
+    var queryStr = `INSERT INTO users
+      (username, firstname, lastname, email)
+      VALUES ('lukie', 'luke', 'wilson', 'l@l.com');`
+
+    pool.query(queryStr, function(err, result) {
+      if (err) return console.log(err);
+      console.log('success');
+      console.log(result)
+      res.send("user created");
+    })
   }
-
 }
