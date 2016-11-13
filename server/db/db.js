4c62a560cc359f987d9de0a4043d293763bc7180
Luke Wilson <wilson.palooza@gmail.com>
2016-11-12 16:02:00 -0800

    - Sets up postgres client pooling using pg-pool - PostgreSQL can only execute 1 query at a time per connected client. Client pooling allows you to use a pool of already connected clients and reuset them for each request to the app.

--- server/db/db.js
+++ server/db/db.js
@@ -1,34 +1,37 @@
 //node-postgres.  Docs link here: https://www.npmjs.com/package/pg
 var pg = require('pg');
-var schema = require('./schema');
+
 // database details - PRIVATE
 var dbConfig = require('./dbConfig.js');
-
+// dbConfig.url = 'postgres://localhost:5432/hells'  // Uncomment this line if you want to test on your local DB.
 
 //set up postgres client pooling
 var config = {
   user: dbConfig.username,
-  database: dbConfig.dbName,
   password: dbConfig.password,
+  host: dbConfig.host,
   port: 5432,
+  database: dbConfig.dbName,
   max: 10, //max clients in the pool
   idleTimeoutMillis: 30000 //how long a client can be idle before being closed
 }
 
-// Uncomment this line if you want to test on your local DB.
-// var dbConfig.url = 'postgres://localhost:5432/hells'
+var pool = new pg.Pool(config);
+module.exports = pool;
 
-// set database location
-var connectionString = process.env.DATABASE_URL || dbConfig.url;
 
-//instantiate a new pg client
-var client = new pg.Client(connectionString);
+var schema = require('./schema');
+// // set database location
+// var connectionString = process.env.DATABASE_URL || dbConfig.url;
 
-//connect to our database
-client.connect();
+// //instantiate a new pg client
+// var client = new pg.Client(connectionString);
 
-//set up the schema
-var query = client.query(schema);
-query.on('end', () => { client.end(); });
+// //connect to our database
+// client.connect();
+
+// //set up the schema - THIS WILL DROP ALL TABLES.
+// var query = client.query(schema);
+// query.on('end', () => { client.end(); });
 
 
