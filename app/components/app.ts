c76e62c311f6991f3f99c4ab8bb6dcbdaffc1bf1
Michael Oliver <jmichaeloliver12@gmail.com>
2016-11-14 09:03:40 -0700

    home folder

--- app/components/app.ts
+++ app/components/app.ts
@@ -5,11 +5,17 @@ import {UIROUTER_DIRECTIVES} from "ui-router-ng2";
   selector: 'my-app',
   template: `
   <div class="nav">
+    {{title}}
     <input class="searchbox" type="text" name="search" placeholder="Dig in the Gear Box...">
+    <button class="searchbutton" type="button">Dig In!</button>
+
     <a class="btn btn-primary" uiSref="Home" uiSrefActive="active">Home</a>
     <a uiSref="gears" uiSrefActive="active">Gears</a>
+    <a class="btn btn-secondary" uiSref="Login" uiSrefActive="active">Login</a>
   </div>
 
   <ui-view></ui-view>
 `})
-export class App { }
+export class App {
+  title: "Gear Box"
+}
