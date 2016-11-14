bd1083eadbdcfc538693e467e5b0f86e5592d230
Daisy Good <daisy.m.good@gmail.com>
2016-11-14 13:05:08 -0600

    add route

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -7,11 +7,14 @@ var userController = require('../users/userController.js');
 module.exports = function(app, express) {
   //User routes
   app.get('/api/users', userController.getAllUsers);
+  app.get('api/users:id', userController.getUserById);
   app.post('/api/users', userController.createUser);
 
   //Product routes
-
-
+  app.get('/api/products', productsController.getAllProducts);
+  app.get('api/products:id', productsController.getProductById);
+  app.post('api/product', productsController.createProduct);
+  
   //Review routes
 
 
