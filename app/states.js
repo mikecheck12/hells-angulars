3ea7aec91044b87bf0462aed99f1057c7e4e9066
Alex Leo <aleo1337@gmail.com>
2016-11-14 22:02:35 -0500

    Remove extra built files in wrong location

--- app/states.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var home_1 = require("./components/home/home");
-var products_component_1 = require("./components/products/products.component");
-/** States */
-exports.homeState = { name: 'home', url: '/', component: home_1.Home };
-exports.gearState = { name: 'gear', url: '/gear', component: products_component_1.ProductsComponent };
