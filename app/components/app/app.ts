c63c85f91b52bfdea2ad57be397d03be87f3e8d9
Alex Leo <aleo1337@gmail.com>
2016-11-14 19:26:56 -0500

    Import auth service and add to constructor

--- app/components/app/app.ts
+++ app/components/app/app.ts
@@ -1,6 +1,6 @@
 import {Component} from "@angular/core";
 import {UIROUTER_DIRECTIVES} from "ui-router-ng2";
-
+import {Auth} from "../../auth/auth.service";
 @Component({
   moduleId: module.id,
   selector: 'my-app',
@@ -10,18 +10,22 @@ import {UIROUTER_DIRECTIVES} from "ui-router-ng2";
     <input class="searchbox" type="text" name="search" placeholder="Dig in the Gear Box...">
     <button class="btn btn-primary" type="button">Dig In!</button>
 
-    <a class="btn btn-primary" uiSref="Home" uiSrefActive="active">Home</a>
-    <a uiSref="gears" uiSrefActive="active">Gears</a>
-    <a class="btn btn-secondary" uiSref="Login" uiSrefActive="active">Login</a>
+    <a class="btn btn-primary" uiSref="home" uiSrefActive="active">Home</a>
+    <a uiSref="gear" uiSrefActive="active">Gear</a>
+    <button class="btn btn-primary btn-margin" (click)="auth.login()" *ngIf="!auth.authenticated()">Log In</button>
+    <button class="btn btn-primary btn-margin" (click)="auth.logout()" *ngIf="auth.authenticated()">Log Out</button>
   </div>
 
   <ui-view></ui-view>
 
   <div>Footer</div>
   `,
-  styleUrls: [ 'app.css' ]
+  styleUrls: [ 'app.css' ],
+  providers: [ Auth ]
 })
 
 export class App {
-  title: "Gear Box"
+  title: "Gear Box";
+
+  constructor(private auth: Auth) {}
 }
