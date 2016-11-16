68b403696876280fb08bf038860fd83702ca15cf
Luke Wilson <wilson.palooza@gmail.com>
2016-11-16 14:52:35 -0800

    - Creates a dummy data file containing fake responses for getUserInfo calls

---
+++ app/data/dummyusers.ts
@@ -0,0 +1,65 @@
+export const UsersData = [
+  {
+    "id": 2,
+    "username": "test@test.com",
+    "password": "$2a$10$wY5gBNUMjHo6yczWLiQ4JeQgo3.sZTuNf3YrSk7url8V71UC1pBpu",
+    "firstname": "test",
+    "lastname": "test",
+    "email": "test@test.com",
+    "location": null,
+    "profilepic": null
+  },
+  {
+    "id": 3,
+    "username": "alex@alex.com",
+    "password": "$2a$10$yQJc/KhUMEL4nIUAOU6Z2O71xEe4NEvxhHaflSt5C0B40UPWM76NK",
+    "firstname": "test",
+    "lastname": "test",
+    "email": "alex@alex.com",
+    "location": null,
+    "profilepic": null
+  },
+  {
+    "id": 13,
+    "username": "test1@test1.com",
+    "password": "$2a$10$Ty4T9vldHU/byFXzUf5w3.WOJxg4xZZG2Sm0Afln55pzMlraEHO.u",
+    "firstname": "test1",
+    "lastname": "test1",
+    "email": "test1@test1.com",
+    "location": null,
+    "profilepic": null
+  },
+  {
+    "id": 15,
+    "username": "test2@test2.com",
+    "password": "$2a$10$/nbG1vU2Ej4xl2eDh05oL..XLOesAyO8Lndt4f6r9KdPrqwMSsEwm",
+    "firstname": "test2",
+    "lastname": "test2",
+    "email": "test2@test2.com",
+    "location": null,
+    "profilepic": null
+  },
+  {
+    "id": 19,
+    "username": "test3@test3.com",
+    "password": "$2a$10$GtPCVaBwma5WLyOgvxGLDOJYYx0FXRRCmXDckvn8nDVtaPz6y2BC2",
+    "firstname": "test3",
+    "lastname": "test3",
+    "email": "test3@test3.com",
+    "location": null,
+    "profilepic": null
+  }
+];
+
+export const UserData = [
+  {
+    "id": 2,
+    "username": "test@test.com",
+    "password": "$2a$10$wY5gBNUMjHo6yczWLiQ4JeQgo3.sZTuNf3YrSk7url8V71UC1pBpu",
+    "firstname": "test",
+    "lastname": "test",
+    "email": "test@test.com",
+    "location": null,
+    "profilepic": null
+  }
+];
