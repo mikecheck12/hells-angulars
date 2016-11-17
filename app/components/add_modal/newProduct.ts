4435db4a4152b36699e7df07021ee1742aee5371
Alex Leo <aleo1337@gmail.com>
2016-11-17 18:07:29 -0500

    Remove unused contructor

--- app/components/add_modal/newProduct.ts
+++ app/components/add_modal/newProduct.ts
@@ -1,11 +1,9 @@
 export class NewProduct {
-  constructor(
-    public id: number,
-    public productName: string,
-    public pricePerDay: number,
-    public ownerId: number,
-    public categoryId: number,
-    public location?: string,
-    public imageLink?: string
-  )
+    public id: number;
+    public productName: string;
+    public pricePerDay: number;
+    public ownerId: number;
+    public categoryId: number;
+    public location?: string;
+    public imageLink?: string;
 }
