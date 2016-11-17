c8048d9a8dd3458b34ecb631cc18faf6d882e3fb
Luke Wilson <wilson.palooza@gmail.com>
2016-11-17 00:15:57 -0800

    - Add cors middleware to server/app.js

--- server/app.js
+++ server/app.js
@@ -2,13 +2,19 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 var logger = require('morgan');
-
+var cors = require('cors');
 
 var app = express();
+
+app.use(cors());
+
 //to parse incoming requests so we can access req.body.whatever
 app.use(bodyParser.json());
 
+// Robin's logger for incoming requests
 app.use(function(req, res, next) {   console.log(     `${Date()} ${req.method} ${req.url}     ${req.body}`);   next(); });
+
+
 
 var __projectRoot = path.join(__dirname, '../');
 app.use(express.static(__projectRoot));
