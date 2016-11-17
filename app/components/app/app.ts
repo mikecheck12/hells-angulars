9c28ab1c1ff5f32efc8f6ecf2596792f0083b825
Alex Leo <aleo1337@gmail.com>
2016-11-17 18:52:54 -0500

    Fix linting errors and style code appropriately

--- app/components/app/app.ts
+++ app/components/app/app.ts
@@ -1,38 +1,33 @@
-import {Component, OnInit} from "@angular/core";
-import {UIROUTER_DIRECTIVES} from "ui-router-ng2";
-import {Auth} from "../../auth/auth.service";
-
-import { ProductsService } from '../products/products.service';
-
+import { Auth }                from "../../auth/auth.service";
+import { Component, OnInit }   from "@angular/core";
+import { ProductsService }     from "../products/products.service";
+import { UIROUTER_DIRECTIVES } from "ui-router-ng2";
 
 @Component({
   moduleId: module.id,
-  selector: 'my-app',
-  providers: [Auth],
-  templateUrl: 'app.html',
-  styleUrls: [ 'app.css' ]
-
+  selector: "my-app",
+  providers: [ Auth ],
+  templateUrl: "app.html",
+  styleUrls: [ "app.css" ],
 })
 
 export class App {
 
   title: "Gear Box";
 
-
   constructor(
     private auth: Auth,
     private productsService: ProductsService
    ) {}
 
-  public onSearch(form:any) {
+  public onSearch(form: any) {
     console.log(form.value.keyword);
     this.productsService.keyword = form.value.keyword;
 
   };
 
   ngOnInit(): void {
-    this.auth.findOrCreateUser(localStorage.getItem('profile'));
+    this.auth.findOrCreateUser(localStorage.getItem("profile"));
   }
-
 
 }
