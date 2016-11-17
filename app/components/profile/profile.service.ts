f8671dd0298833771067235ba6dfabde3ef5a923
Luke Wilson <wilson.palooza@gmail.com>
2016-11-16 23:14:58 -0800

    - Add to non-dummyData getUserInfo version (NOT WORKING YET)

--- app/components/profile/profile.service.ts
+++ app/components/profile/profile.service.ts
@@ -9,17 +9,27 @@ import { UserData } from "../../data/dummyusers";
 @Injectable()
 
 export class ProfileService {
+  username: string;
+
   constructor(
     private http: Http
   ) {}
 
   // need to write this for when the DB is populated
   public getUserInfo(): Promise<any> {
-    console.log("getUserInfo from service fired");
+    // dummy data version
     return Promise.resolve(UserData);
+
+    // live database version
+    // console.log("YO", JSON.parse(localStorage.getItem("profile"));)
+    // return this.http.get(`/api/profile/`)
+    //   .toPromise()
+    //   .then(response => console.log(response))
+    //   .catch(this.handleError);
+
   }
 
-  // private handleError(error: any): Promise<any> {
-  //   return Promise.reject(error.message || error);
-  // }
+  private handleError(error: any): Promise<any> {
+    return Promise.reject(error.message || error);
+  }
 }
