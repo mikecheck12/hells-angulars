322c684caa8d3a96d8076a8c17b34992e173d7c5
Luke Wilson <wilson.palooza@gmail.com>
2016-11-17 00:13:45 -0800

    - Import AuthHttp - Refactor get request for getUserInfo to use AuthHttp instead of Http for in profile.service.ts

--- app/components/profile/profile.service.ts
+++ app/components/profile/profile.service.ts
@@ -1,31 +1,32 @@
 import { Injectable } from "@angular/core";
 // import { Headers, Http } from "@angular/http";
 import { Http } from "@angular/http";
+import { AuthHttp } from "angular2-jwt";
 
 import "rxjs/add/operator/toPromise";
+
 
 import { UserData } from "../../data/dummyusers";
 
 @Injectable()
 
 export class ProfileService {
-  username: string;
 
   constructor(
-    private http: Http
+    private http: Http,
+    private authHttp: AuthHttp
   ) {}
 
   // need to write this for when the DB is populated
   public getUserInfo(): Promise<any> {
     // dummy data version
-    return Promise.resolve(UserData);
+    // return Promise.resolve(UserData);
 
     // live database version
-    // console.log("YO", JSON.parse(localStorage.getItem("profile"));)
-    // return this.http.get(`/api/profile/`)
-    //   .toPromise()
-    //   .then(response => console.log(response))
-    //   .catch(this.handleError);
+    return this.authHttp.get(`/api/users/`)
+      .toPromise()
+      .then(response => console.log(response))
+      .catch(this.handleError);
 
   }
 
