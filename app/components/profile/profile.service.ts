d956c96f4ffcba70ddbc714bf1fe058ed0ce530c
Luke Wilson <wilson.palooza@gmail.com>
2016-11-16 21:21:59 -0800

    - Fix most linting errors for profile.service.ts

--- app/components/profile/profile.service.ts
+++ app/components/profile/profile.service.ts
@@ -1,24 +1,25 @@
-import { Injectable } from '@angular/core';
-import { Headers, Http } from '@angular/http';
+import { Injectable } from "@angular/core";
+// import { Headers, Http } from "@angular/http";
+import { Http } from "@angular/http";
 
-import 'rxjs/add/operator/toPromise';
+import "rxjs/add/operator/toPromise";
 
-import { UserData } from '../../data/dummyusers';
+import { UserData } from "../../data/dummyusers";
 
 @Injectable()
 
 export class ProfileService {
   constructor(
     private http: Http
-  ){}
+  ) {}
 
-  //need to write this for when the DB is populated
-  public getUserInfo(): Promise<any>{
-    console.log("getUserInfo from service fired")
-    return Promise.resolve(UserData)
+  // need to write this for when the DB is populated
+  public getUserInfo(): Promise<any> {
+    console.log("getUserInfo from service fired");
+    return Promise.resolve(UserData);
   }
 
-  private handleError(error: any): Promise<any> {
-    return Promise.reject(error.message || error);
-  }
+  // private handleError(error: any): Promise<any> {
+  //   return Promise.reject(error.message || error);
+  // }
 }
