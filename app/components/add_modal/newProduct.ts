e3872ecab36261543b20831de5ad1c56334d1d66
Alex Leo <aleo1337@gmail.com>
2016-11-19 16:42:11 -0500

    Remove duplicate userId property

--- app/components/add_modal/newProduct.ts
+++ app/components/add_modal/newProduct.ts
@@ -1,11 +1,10 @@
 export class NewProduct {
     public id: number;
-    public userId: number;
+    public userId: string;
     public productName: string;
     public pricePerDay: number;
     public ownerId: number;
     public categoryId: number;
     public location?: string;
     public imageLink?: string;
-    public userId: string;
 }
