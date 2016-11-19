a99c6cac3d1d3e22c388d97a78098c42e5c9b560
Alex Leo <aleo1337@gmail.com>
2016-11-19 17:50:43 -0500

    Edit example test for server

--- server/test/test.js
+++ server/test/test.js
@@ -1,8 +1,9 @@
 var assert = require('assert');
 describe('Array', function() {
-  describe('#indexOf()', function() {
+  describe('#indexOf() - this is a test test to test the tests', function() {
     it('should return -1 when the value is not present', function() {
       assert.equal(-1, [1,2,3].indexOf(4));
     });
   });
 });
+
