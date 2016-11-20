542ccd20f0d744608c6de10e98d432b5a33f1ccb
Alex Leo <aleo1337@gmail.com>
2016-11-19 19:21:22 -0500

    Add sample back end test

--- server/test/test.js
+++ server/test/test.js
@@ -1,10 +1,17 @@
 var assert = require('assert');
+var expect = require('chai').expect;
 describe('Array', function() {
   describe('#indexOf() - this is a test test to test the tests', function() {
     it('should return -1 when the value is not present', function() {
       assert.equal(-1, [1,2,3].indexOf(4));
     });
   });
+});
+
+describe("A test suite", function() {
+   beforeEach(function() { });
+   afterEach(function() { });
+   it('should pass', function() { expect(true).to.be.true; });
 });
 
 // describe('Server', function() {
