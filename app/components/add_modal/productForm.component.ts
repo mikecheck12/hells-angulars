dbcec362e3e3c78c9a6c49c177f09682cf1c3807
Luke Wilson <wilson.palooza@gmail.com>
2016-11-21 13:58:57 -0800

    - Add google places autocomplete - Adjust object that is sent to server by adding info from google places location

--- app/components/add_modal/productForm.component.ts
+++ app/components/add_modal/productForm.component.ts
@@ -1,4 +1,7 @@
-import { Component, Output, EventEmitter }         from "@angular/core";
+import { Component, OnInit, ViewChild, Output, EventEmitter }         from "@angular/core";
+import { FormControl }                   from "@angular/forms";
+import { MapsAPILoader }                 from 'angular2-google-maps/core';
+
 import { NewProduct }        from "./newProduct";
 import { NewProductService } from "./newProduct.service";
 
@@ -13,15 +16,29 @@ export class NewProductForm {
   @Output()
   close: EventEmitter<any> = new EventEmitter();
 
+  public latitude: number;
+  public longitude: number;
+  public searchControl: FormControl;
+  public zoom: number;
   public categories = [ "Backpacking", "Bike", "Surf", "Snowboard", "Ski", "SUP", "Kayak" ];
-
   public model = new NewProduct();
+  public place: string;
 
-  constructor(private newProductService: NewProductService) { }
+  // Note: This is looking for #search in the HTML template
+  @ViewChild("search")
+  public searchElementRef: ElementRef;
+
+  constructor(
+    private newProductService: NewProductService,
+    private mapsAPILoader: MapsAPILoader
+  ) { }
 
   public onSubmit(model: NewProduct) {
-    // console.log(model);
-    // console.log(this);
+    model.placeId = this.place.place_id;
+    model.city = this.place.address_components[1].long_name;
+    model.state = this.place.address_components[3].short_name;
+    model.zip = this.place.address_components[5].long_name;
+
     model.userId = JSON.parse(localStorage.getItem("profile")).user_id;
     this.newProductService.postProduct(model)
         .then(result => {
@@ -32,4 +49,38 @@ export class NewProductForm {
         });
   };
 
+  ngOnInit() {
+    //create search FormControl
+    this.searchControl = new FormControl();
+
+    // //set current position
+    // this.setCurrentPosition();
+
+    //load Places Autocomplete
+    this.mapsAPILoader.load().then(() => {
+      let autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement, {
+        types: ["address"]
+      });
+      autocomplete.addListener("place_changed", () => {
+        //get the place result
+        let place: google.maps.places.PlaceResult = autocomplete.getPlace();
+        console.log("place -->", place);
+        this.place = place;
+
+        //set latitude and longitude
+        this.latitude = place.geometry.location.lat();
+        this.longitude = place.geometry.location.lng();
+      });
+    });
+  }
+
+  // private setCurrentPosition() {
+  //   if ("geolocation" in navigator) {
+  //     navigator.geolocation.getCurrentPosition((position) => {
+  //       this.latitude = position.coords.latitude;
+  //       this.longitude = position.coords.longitude;
+  //       this.zoom = 12;
+  //     });
+  //   }
+  // }
 }
