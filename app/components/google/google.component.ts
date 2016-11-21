b91b96f69f21ed0b76569e9e76a08f01b305a2d3
Luke Wilson <wilson.palooza@gmail.com>
2016-11-21 14:52:19 -0800

    - clean up code in google.component.ts - fix tslint error

--- app/components/google/google.component.ts
+++ app/components/google/google.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
 import { FormControl } from "@angular/forms";
-import { MapsAPILoader } from 'angular2-google-maps/core';
+import { MapsAPILoader } from "angular2-google-maps/core";
 
 @Component({
   moduleId: module.id,
@@ -25,27 +25,27 @@ export class Google {
   ) {}
 
   ngOnInit() {
-    //set google maps defaults
+    // set google maps defaults
     this.zoom = 4;
     this.latitude = 39.8282;
     this.longitude = -98.5795;
 
-    //create search FormControl
+    // create search FormControl
     this.searchControl = new FormControl();
 
-    //set current position
+    // set current position
     this.setCurrentPosition();
 
-    //load Places Autocomplete
+    // load Places Autocomplete
     this.mapsAPILoader.load().then(() => {
       let autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement, {
-        types: ["address"]
+        types: ["address"],
       });
       autocomplete.addListener("place_changed", () => {
-        //get the place result
+        // get the place result
         let place: google.maps.places.PlaceResult = autocomplete.getPlace();
         console.log(place);
-        //set latitude and longitude
+        // set latitude and longitude
         this.latitude = place.geometry.location.lat();
         this.longitude = place.geometry.location.lng();
       });
